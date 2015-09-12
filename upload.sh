21d74b07e4422dc092185d4954da875f7938df67
Elians <wjybluse@users.noreply.github.com>
2015-09-12 15:03:13 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-
-BASEDIR=$(cd "$(dirname "$0")"; pwd)
-
-cd $BASEDIR
-
-cmd=`cat config.ini |xargs sh -x ./upload.sh`
-
-if [ $? -ne 0 ]; then
-    echo $cmd
-    exit 1
-fi
-echo $cmd
-
-[vagrant@vagrant-centos65 test]$ cat upload.sh
-#!/bin/bash
 BASEDIR=$(dirname $0)
 HOST=$1
 PORT=$2
